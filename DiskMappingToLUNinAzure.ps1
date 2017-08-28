483920f889ffc40927be72b9f7e3aad4bcedc3d4
Sanjaya Padhi <sanjayapadhi@gmail.com>
2017-08-27 19:53:46 -0700

    Update DiskMappingToLUNinAzure.ps1
    Updated the script to keep only the portion that needs to be run on the machine locally to get the disk status.

--- DiskMappingToLUNinAzure.ps1
+++ DiskMappingToLUNinAzure.ps1
@@ -3,21 +3,6 @@ Login-AzureRmAccount
 $subscriptionname = Get-AzureRmSubscription | Out-GridView -Title "Select subscription for the activity..." -PassThru
 Select-AzureRmSubscription -SubscriptionName $subscriptionname.SubscriptionName
 
-$disklist = Get-AzureRmVM -ResourceGroupName AZ3RGPOBIDAP -Name AZ3OBIPDAPSQL01 -Status 
-$disklist | select -ExpandProperty disks 
-
-
-
-
-
-get-azurevm -Name AZ3OBIPDAPSQL01 | Get-AzureDataDisk | ft MediaLink, Lun
-get-disk  | format-list number, path
-
-get-AzureRmVm -ResourceGroupName yourResourceGroupName -Name yourVmName
-
-Get-AzureVM -name AZ3OBIPDAPSQL01
-
-
 # locally on the server run this to find the mapping to the Azure LUN number
 $disksNumber = get-disk #| select Number 
 foreach ($disk in $disksNumber.number)
