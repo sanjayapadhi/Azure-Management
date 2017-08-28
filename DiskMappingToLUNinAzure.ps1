f39f67c5433e116d5c27baefac34ac6b197df4f4
Sanjaya Padhi <sanjayapadhi@gmail.com>
2017-08-27 19:50:59 -0700

    Add files via upload

---
+++ DiskMappingToLUNinAzure.ps1
@@ -0,0 +1,33 @@
+﻿
+Login-AzureRmAccount
+$subscriptionname = Get-AzureRmSubscription | Out-GridView -Title "Select subscription for the activity..." -PassThru
+Select-AzureRmSubscription -SubscriptionName $subscriptionname.SubscriptionName
+
+$disklist = Get-AzureRmVM -ResourceGroupName AZ3RGPOBIDAP -Name AZ3OBIPDAPSQL01 -Status 
+$disklist | select -ExpandProperty disks 
+
+
+
+
+
+get-azurevm -Name AZ3OBIPDAPSQL01 | Get-AzureDataDisk | ft MediaLink, Lun
+get-disk  | format-list number, path
+
+get-AzureRmVm -ResourceGroupName yourResourceGroupName -Name yourVmName
+
+Get-AzureVM -name AZ3OBIPDAPSQL01
+
+
+# locally on the server run this to find the mapping to the Azure LUN number
+$disksNumber = get-disk #| select Number 
+foreach ($disk in $disksNumber.number)
+{
+    if ((get-disk -Number $disk | select FriendlyName) -match 'Microsoft Virtual Disk' )
+    {
+     $diskdetails = get-disk -Number $disk
+     $DiskH = "0x"+((get-disk -Number $disk | select path) -split "#" )[2]
+     $diskLUN = [convert]::ToInt64($DiskH, 16)
+     Write-Host ("Mapping is - Disk= "+$disk +" -  LUN= "+$diskLUN)
+    }   
+}
+
